9cdcef4a587c16cc58c19e7f3b9f668123bf7606
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-08 23:45:31 -0500

    Updated LoginForm.js to fix bug during build

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,25 +1,31 @@
+// Import React, useState, and several components from react-bootstrap library
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-import { useMutation, gql } from "@apollo/client"; // import useMutation and gql
-import { LOGIN_USER } from "../utils/mutations"; // import LOGIN_USER mutation
-import Auth from "../utils/auth";
 
+// Import Auth and useMutation hook from Apollo Client, as well as LOGIN_USER mutation
+import Auth from "../utils/auth";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+
+// Define LoginForm component
 const LoginForm = () => {
+  // Declare state variables with useState hook
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  // Declare state variables with useMutation hook for login mutation and loading status
+  const [login, { loading }] = useMutation(LOGIN_USER);
 
-  const [loginUser, { err }] = useMutation(LOGIN_USER); // use useMutation to execute the mutation
-
+  // Define function to update state when user input changes
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  // Define function to handle form submission
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -27,22 +33,30 @@ const LoginForm = () => {
     }
 
     try {
-      const { data } = await loginUser({
-        variables: { ...userFormData },
+      // Use login mutation to log user in and store token in Auth
+      const { data } = await login({
+        variables: userFormData,
       });
-
       Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
 
+    // Reset form data after submission
     setUserFormData({
+      username: "",
       email: "",
       password: "",
     });
   };
 
+  // If loading, return loading message
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // Render login form
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
@@ -54,7 +68,7 @@ const LoginForm = () => {
         >
           Something went wrong with your login credentials!
         </Alert>
-        <Form.Group className="mb-3">
+        <Form.Group>
           <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control
             type="text"
@@ -69,7 +83,7 @@ const LoginForm = () => {
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-3">
+        <Form.Group>
           <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control
             type="password"
